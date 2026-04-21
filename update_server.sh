5e7666b43c595f91076dc53a99e2612f52408eed
Nikolai <nikolai@insidedesk.com>
2026-04-20 21:15:57 -0600

    add update script that might work

---
+++ update_server.sh
@@ -0,0 +1,19 @@
+# /bin/sh
+
+# shut down the server while things are being changed
+service apache2 stop
+cd /home/pi/lizard-52.github.io
+git pull
+
+cd /var/www/html
+
+rm -rf ./*
+
+sudo cp -r /home/pi/lizard-52.github.io .
+ln -s ./lizard-52.github.io/* .
+rm -rf ./lizard-52.github.io/.git
+
+sudo find /var/www/html -type f -exec chmod 644 {} \;
+sudo find /var/www/html -type d -exec chmod 755 {} \;
+
+service apache2 start
