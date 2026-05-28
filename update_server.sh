9e6b18ec24a6215e50509732348072bddc5f5a81
Nikolai <nikolai@insidedesk.com>
2026-05-28 17:40:27 -0600

    consider phlebas

--- update_server.sh
+++ update_server.sh
@@ -6,13 +6,13 @@ service apache2 stop
 cd /var/www/html
 
 #rm -rf ./*
-sudo chmod -R 777 ./*
+chmod -R 777 ./*
 
-sudo cp -ru /home/pi/lizard-52.github.io .
+cp -ru /home/pi/lizard-52.github.io .
 ln -s ./lizard-52.github.io/* .
 rm -rf ./lizard-52.github.io/.git
 
-sudo find /var/www/html -type f -exec chmod 644 {} \;
-sudo find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html -type f -exec chmod 644 {} \;
+find /var/www/html -type d -exec chmod 755 {} \;
 
 service apache2 start
